c2931bf09120ccf0a04852cfc23c4f1b73379cf9
Randall Mardus <randalljmardus@gmail.com>
2016-03-08 10:41:38 -0500

    initial UI setup

---
+++ BitList/AddToDoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AddToDoTableViewCell.swift
+//  BitList
+//
+//  Created by Randall Mardus on 3/8/16.
+//  Copyright © 2016 Randall Mardus. All rights reserved.
+//
+
+import UIKit
+
+class AddToDoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ BitList/ToDoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ToDoTableViewCell.swift
+//  BitList
+//
+//  Created by Randall Mardus on 3/8/16.
+//  Copyright © 2016 Randall Mardus. All rights reserved.
+//
+
+import UIKit
+
+class ToDoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
